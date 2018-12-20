61dccdacc2c4eca0b10101d3644b7f36b33ad2d2
Casttle <cberry1222@gmail.com>
2018-12-19 23:23:21 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import React
-// , { Component } 
-from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Header from "./components/header";
-// import SearchBox from "./components/searchBox";
-// import ResultsCard from "./components/Results";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch"
 import './App.css';
-
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="container">
-//           <Header></Header>
-//           <SearchBox></SearchBox>
-//           <div className="results">
-//             <h2>Results</h2>
-//             <ResultsCard></ResultsCard>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
 
 
 function App() {
